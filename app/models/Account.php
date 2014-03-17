2f46db8356631630bf13505c568b5ade6bb96024
Eftakhairul Islam <eftakhairul@gmail.com>
2014-03-17 10:39:51 -0400

    Account Entity is updated

--- app/models/Account.php
+++ app/models/Account.php
@@ -39,6 +39,38 @@ class Account
      */
     private $isActive;
 
+    /**
+     * @var string $type
+     *
+     * @Column(name="type", type="varchar", columnDefinition="ENUM('Saving', 'checking', 'Investment')")
+     */
+    private $type;
+
+
+    /**
+     * @var $user
+     *
+     * @ManyToOne(targetEntity="User")
+     * @JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+
+    /**
+     * @var \DateTime $createDate
+     *
+     * @ORM\Column(name="create_date", type="date", nullable=false)
+     */
+    private $createDate;
+
+    /**
+     * @var \DateTime $modifyDate
+     *
+     * @ORM\Column(name="update_date", type="datetime", nullable=false)
+     */
+    private $updateDate;
+
+
     
     /**
      * Get id
@@ -48,5 +80,77 @@ class Account
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @param $accountNo
+     * @return $this
+     */
+    public function setAccountNo($accountNo)
+    {
+        $this->accountNo = $accountNo;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccountNo()
+    {
+        return $this->accountNo;
+    }
+
+    /**
+     * @param string $interestRate
+     * @return $this
+     */
+    public function setInterestRate($interestRate)
+    {
+        $this->interestRate = $interestRate;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInterestRate()
+    {
+        return $this->interestRate;
+    }
+
+    /**
+     * @param string $isActive
+     * @return $this
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param string $type
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
     }
 }
