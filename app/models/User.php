b471690526ab043795cdc40d58d5daf988dddcd7
Eftakhairul Islam <eftakhairul@gmail.com>
2014-03-18 19:37:20 -0400

    Getter and setter methods are added to User and Card entity

--- app/models/User.php
+++ app/models/User.php
@@ -68,4 +68,112 @@ class User
     {
         $this->accounts = new ArrayCollection();
     }
+
+    /**
+     * @param string $accounts
+     * @return $this;
+     */
+    public function setAccounts($accounts)
+    {
+        $this->accounts = $accounts;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccounts()
+    {
+        return $this->accounts;
+    }
+
+    /**
+     * @param string $firstName
+     * @return $this
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $lastName
+     * @return $this
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = md5($password);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $roolId
+     * @return $this
+     */
+    public function setRoolId($roolId)
+    {
+        $this->roolId = $roolId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRoolId()
+    {
+        return $this->roolId;
+    }
+
+    /**
+     * @param string $username
+     * @return $this
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
 }
