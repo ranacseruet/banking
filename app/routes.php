a86e1cfe274ae39ea1e45a7b867d93ab7ce26a86
Eftakhairul Islam <eftakhairul@gmail.com>
2014-04-08 09:38:35 -0400

    Route is updated for card and account controller

--- app/routes.php
+++ app/routes.php
@@ -16,4 +16,6 @@ Route::controller('users', 'UsersController');
 Route::controller('admin', 'AdminController');
 Route::resource('transfer', 'TransferController');
 Route::controller('account', 'AccountController');
+Route::controller('card', 'CardController');
 Route::get('account/create/id/{id}', 'AccountController@getCreate');
+Route::get('card/create/id/{id}', 'CardController@getCreate');
