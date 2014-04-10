be1b126cf92b51ec5dbe88481968639e155e1fb4
Eftakhairul Islam <eftakhairul@gmail.com>
2014-04-10 09:57:19 -0400

    account creation is updated

--- app/views/account/create.blade.php
+++ app/views/account/create.blade.php
@@ -7,17 +7,22 @@
 
 	<div class="form-group">
 		{{ Form::label('account_no', 'Account No.') }}
-		{{ Form::text('account_no',  Input::old('account_no'), array('class' => 'form-control')) }}
+		{{ Form::text('account_no',  Input::old('account_no', $account_no), array('class' => 'form-control')) }}
 	</div>
 
 	<div class="form-group">
 		{{ Form::label('type', 'Account Type') }}
-		{{ Form::select('type', $type, Input::old('type'), array('class' => 'form-control')) }}
+		{{ Form::select('type', $type, Input::old('type'), array('class' => 'form-control', 'id'=>'type')) }}
 	</div>
 
-     <div class="form-group">
+     <div class="form-group" id="interest_rate_div">
 		{{ Form::label('interest_rate', 'Interest Rate %') }}
 		{{ Form::text('interest_rate', Input::old('interest_rate'), array('class' => 'form-control')) }}
+	 </div>
+
+    <div class="form-group" id = 'amount_div' style="display: none">
+		{{ Form::label('amount', 'Fixed Amount') }}
+		{{ Form::text('amount', Input::old('amount'), array('class' => 'form-control')) }}
 	 </div>
      <input type="hidden" name='user_id' value="{{$user_id}}" />
 
@@ -25,3 +30,20 @@
 
 {{ Form::close() }}
 
+<script type="text/javascript">
+    $('#type').on('change', function(){
+        var type = $('#type option:selected').val();
+
+        if(type == 'checking' || type == 'credit_card') {
+            $('#interest_rate_div').hide();
+        } else {
+            $('#interest_rate_div').show();
+        }
+
+        if(type == 'investment') {
+            $('#amount_div').show();
+        } else {
+            $('#amount_div').hide();
+        }
+    });
+</script>
